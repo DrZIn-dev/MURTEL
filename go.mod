664540e233e2133811ed8c6d18e97b84d05e34a7
Kameeeyo <kameeeyo@gmail.com>
2021-02-01 10:52:21 +0700

    init base models , before create , after update #2

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber/v2 v2.4.0
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	gorm.io/driver/postgres v1.0.7
 	gorm.io/gorm v1.20.12
---
+++ models/models.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Base struct {
+	UUID     uuid.UUID `json:"_id" gorm:"primaryKey;autoIncrement:false;unique"`
+	CreateAt string    `json:"create_at"`
+	UpdateAt string    `json:"update_at"`
+}
+
+func GenerateISOString() string {
+	return time.Now().UTC().Format("2006-01-02T15:04:05.999Z07:00")
+}
+
+func (base *Base) BeforeCreate(tx *gorm.DB) error {
+	base.UUID = uuid.New()
+
+	t := GenerateISOString()
+	base.CreateAt, base.UpdateAt = t, t
+
+	return nil
+}
+
+func (base *Base) AfterUpdate(tx *gorm.DB) error {
+	base.UpdateAt = GenerateISOString()
+	return nil
+}
