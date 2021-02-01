651a44c4f0e2ad4c0f2445c696ce03d435299f54
Kameeeyo <kameeeyo@gmail.com>
2021-02-01 10:47:13 +0700

    init cors and server #2

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/DrZIn-dev/EXISTING-Hotel-manage-back-end
 go 1.15
 
 require (
+	github.com/gofiber/fiber/v2 v2.4.0
 	github.com/joho/godotenv v1.3.0
 	gorm.io/driver/postgres v1.0.7
 	gorm.io/gorm v1.20.12
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/DrZIn-dev/EXISTING-Hotel-manage-back-end/database"
+import (
+	"github.com/DrZIn-dev/EXISTING-Hotel-manage-back-end/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"log"
+	"os"
+)
+
+func CreateServer() *fiber.App {
+	app := fiber.New()
+	return app
+}
 
 func main() {
 	database.ConnectDB()
+	app := CreateServer()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	port := os.Getenv("PORT")
+	log.Fatal(app.Listen(":" + port))
 }
