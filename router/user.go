3bd48eba518af75ead9cf80e9bf20ea789e68a43
Kameeeyo <kameeeyo@gmail.com>
2021-02-02 10:17:30 +0700

    login with validator and password hash compare #3

--- router/user.go
+++ router/user.go
@@ -4,6 +4,7 @@ import (
 	db "github.com/DrZIn-dev/EXISTING-Hotel-manage-back-end/database"
 	"github.com/DrZIn-dev/EXISTING-Hotel-manage-back-end/models"
 	"github.com/DrZIn-dev/EXISTING-Hotel-manage-back-end/util"
+	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 	"math/rand"
@@ -11,6 +12,7 @@ import (
 
 func SetupUserRoutes() {
 	USER.Post("/signup", CreateUser)
+	USER.Post("/signin", LoginUser)
 }
 
 func CreateUser(c *fiber.Ctx) error {
@@ -62,6 +64,44 @@ func CreateUser(c *fiber.Ctx) error {
 	accessToken, refreshToken := util.GenerateTokens(u.UUID.String())
 	accessCookie, refreshCookie := util.GetAuthCookies(accessToken, refreshToken)
 
+	c.Cookie(accessCookie)
+	c.Cookie(refreshCookie)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"access_token":  accessToken,
+		"refresh_token": refreshToken,
+	})
+}
+
+func LoginUser(c *fiber.Ctx) error {
+	type LoginInput struct {
+		Identity string `json:"identity" valid:"required"`
+		Password string `json:"password" valid:"required"`
+	}
+
+	input := new(LoginInput)
+	err := c.BodyParser(input)
+	if err != nil {
+		return c.JSON(fiber.Map{"error": true, "input": "Please review your input"})
+	}
+	_, err = govalidator.ValidateStruct(input)
+	if err != nil {
+		return c.JSON(fiber.Map{"error": err.Error()})
+	}
+
+	u := new(models.User)
+	res := db.DB.Where(&models.User{Email: input.Identity}).Or(&models.User{Username: input.Identity}).First(&u)
+	if res.RowsAffected <= 0 {
+		return c.JSON(fiber.Map{"error": true, "general": "Invalid Credentials."})
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(input.Password))
+	if err != nil {
+		return c.JSON(fiber.Map{"error": true, "general": "Invalid Credentials."})
+	}
+
+	accessToken, refreshToken := util.GenerateTokens(u.UUID.String())
+	accessCookie, refreshCookie := util.GetAuthCookies(accessToken, refreshToken)
 	c.Cookie(accessCookie)
 	c.Cookie(refreshCookie)
 
