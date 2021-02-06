83ea02c5e6e972c4bc00a9ff6d9fdcc557093b46
Kameeeyo <kameeeyo@gmail.com>
2021-02-06 17:01:10 +0700

    change end point

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -13,7 +13,7 @@ import "@/scss/custom.scss";
 Vue.config.productionTip = false;
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://the-existing-test-back-end.herokuapp.com";
 Vue.use(VueAxios, axios);
 Vue.use(VueCookies);
 
