c37b031cfe82c87a1f8304dc8c6598ed80fe8313
Kameeeyo <kameeeyo@gmail.com>
2021-02-06 17:41:07 +0700

    fix token

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -13,72 +13,78 @@ import Ticket from "@/views/ticket/store";
 Vue.use(Vuex);
 
 const state = {
-  birth_date: "",
-  email: "",
-  username: "",
-  msg: "",
-  title: "",
-  isShow: false
+    birth_date: "",
+    email: "",
+    username: "",
+    msg: "",
+    title: "",
+    isShow: false
 };
 
 const store = {
-  state,
-  getters: {
-    ...make.getters(state)
-  },
-  mutations: {
-    ...make.mutations(state)
-  },
-  actions: {
-    ...make.actions(state),
-    noti({ commit }, payload) {
-      const { msg, title } = payload;
-      commit("msg", msg);
-      commit("title", title);
-      commit("isShow", true);
-      setTimeout(() => {
-        commit("isShow", false);
-      }, 3000);
+    state,
+    getters: {
+        ...make.getters(state)
     },
-    async getUserData({ commit }) {
-      console.log(Vue.$cookies.get("access_token"));
-      if (Vue.$cookies.get("refresh_token")) {
-        if (!Vue.$cookies.get("access_token")) {
-          await this._vm.axios
-            .get("/user/get-access-token")
-            .then(() => {
+    mutations: {
+        ...make.mutations(state)
+    },
+    actions: {
+        ...make.actions(state),
+        noti({ commit }, payload) {
+            const { msg, title } = payload;
+            commit("msg", msg);
+            commit("title", title);
+            commit("isShow", true);
+            setTimeout(() => {
+                commit("isShow", false);
+            }, 3000);
+        },
+        async getUserData({ commit }) {
+            console.log(Vue.$cookies.get("access_token"));
+            await this._vm.axios.get("/user/get-access-token").then(() => {});
+            this._vm.axios.get("/user/private/user").then(({ data }) => {
+                const { username } = data;
+                commit("username", username);
             });
-        }
-        return this._vm.axios.get("/user/private/user").then(({ data }) => {
-          const { username } = data;
-          commit("username", username);
-          return Promise.resolve();
-        });
-      }
+            if (Vue.$cookies.get("refresh_token")) {
+                if (!Vue.$cookies.get("access_token")) {
+                    await this._vm.axios
+                        .get("/user/get-access-token")
+                        .then(() => {});
+                }
+                return this._vm.axios
+                    .get("/user/private/user")
+                    .then(({ data }) => {
+                        const { username } = data;
+                        commit("username", username);
+                        return Promise.resolve();
+                    });
+            }
 
-      return Promise.resolve();
+            return Promise.resolve();
+        },
+        logOut({ commit }) {
+            commit("username", "");
+            Vue.$cookies.remove("access_token");
+            Vue.$cookies.remove("refresh_token");
+        }
     },
-    logOut({ commit }) {
-      commit("username", "");
-      Vue.$cookies.remove("access_token");
-      Vue.$cookies.remove("refresh_token");
+    modules: {
+        Home,
+        Register,
+        Login,
+        Search,
+        Offer,
+        Ticket
     }
-  },
-  modules: {
-    Home,
-    Register,
-    Login,
-    Search,
-    Offer,
-    Ticket
-  }
 };
 
 const VuexStore = updateModules(
-  new Vuex.Store({
-    plugins: [pathify.plugin],
-    ...store
-  })
+    new Vuex.Store({
+        plugins: [pathify.plugin],
+        ...store
+    })
 );
 
 export default VuexStore;
